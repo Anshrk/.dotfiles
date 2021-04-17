d92830071043eb5a218e903a347e05ae2d7d21e7
Anshrk <anshrk.13@gmail.com>
2021-04-17 15:51:14 +0530

    first

---
+++ dotfileSetup.bash
@@ -0,0 +1,15 @@
+rm -rf ~/.config/awesome
+rm -rf ~/.config/fish
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/fish
+rm -rf ~/.local/share/konsole
+rm -rf ~/.vscode
+rm -rf ~/.gitconfig
+
+ln -s ~/.dotfiles/.config/awesome ~/.config/awesome
+ln -s ~/.dotfiles/.config/fish ~/.config/fish
+ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/.dotfiles/.local/share/fish ~/.local/share/fish
+ln -s ~/.dotfiles/.local/share/konsole ~/.local/share/konsole
+ln -s ~/.dotfiles/.vscode ~/.vscode
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
--- programming.bash
+++ programming.bash
@@ -3,37 +3,32 @@ echo "starting app"
 sudo apt -y update
 sudo apt -y upgrade
 
+# neovim
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+
+# code
+sudo apt install software-properties-common apt-transport-https wget
+sudo wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+sudo apt update
+
+sudo apt install -y fish python3 python3-pip nodejs npm neovim code docker.io
+
+# neovim 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
 # fish shell
-sudo apt install -y fish
 sudo chsh -s /usr/bin/fish
 
-# python3
-sudo apt install -y python3
-sudo apt install -y python3-pip
+# virtual env
 sudo python3 -m pip install --user virtualenv
 
-# node
-sudo apt install -y nodejs
-sudo apt install -y npm
+# node/yarn
 sudo npm install -g yarn
 
 # neovim
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt update
-sudo apt install -y neovim
-
-# VSCode
-sudo apt install software-properties-common apt-transport-https wget
-sudo wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install -y code
-
-# git
-sudo apt install -y git
-
-# docker
-sudo apt install -y docker.io
-
-# final update and upgrade
 sudo apt update
 sudo apt upgrade
