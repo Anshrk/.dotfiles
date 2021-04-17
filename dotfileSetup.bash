b534fe04bb00bc23c207c96f4b4c8276c1e32391
Anshrk <anshrk.13@gmail.com>
2021-04-17 16:14:38 +0530

    sigh, wow

--- dotfileSetup.bash
+++ dotfileSetup.bash
@@ -1,13 +1,17 @@
 rm -rf ~/.config/awesome
 rm -rf ~/.config/fish
 rm -rf ~/.config/nvim
+rm -rf ~/.config/Code
 rm -rf ~/.local/share/fish
 rm -rf ~/.local/share/konsole
 rm -rf ~/.vscode
 rm -rf ~/.gitconfig
 
-mkdir ~/.config
 
+ln -s ~/.dotfiles/.config/Code ~/.config/Code
+ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/.dotfiles/.config/fish ~/.config/fish
+ln -s ~/.dotfiles/.config/awesome ~/.config/awesome
 ln -s ~/.dotfiles/.local/share/fish ~/.local/share/fish
 ln -s ~/.dotfiles/.local/share/konsole ~/.local/share/konsole
 ln -s ~/.dotfiles/.vscode ~/.vscode
